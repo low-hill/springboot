99536df6c72aa86d85314fd772e2e482016312c7
zykim-sph <zykim@sphinfo.co.kr>
2021-02-08 22:34:44 +0900

    add interceptor
    - add timeInterceptor in webmvcconfigurer

---
+++ restapi-test/src/main/java/com/example/demo/config/webConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo.config;
+
+import com.example.demo.interceptor.TimeInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class webConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private TimeInterceptor timeInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(timeInterceptor);
+    }
+}
